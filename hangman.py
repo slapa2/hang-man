01033a2dbfcb1e8f8b50ea20b94a9b3c75d76e05
slawek <slawomirpawlowski@gmail.com>
2018-08-09 21:41:10 +0200

    code clean

--- hangman.py
+++ hangman.py
@@ -1,161 +1,163 @@
 import os
 import random
 
+
 class Game:
-	def __init__(self):
-		self.winResult = 2
-		self.maxMishits = 9
-		self.gameStart()
+    def __init__(self):
+        self.winResult = 2
+        self.maxMishits = 9
+        self.result = [0, 0]
+        self.player = 0
+        self.win = False
+        self.gameStart()
 
-	def gameStart(self):
+    def gameStart(self):
 
-		self.result = [0,0]
-		self.player = 0
-		self.win = False
+        while not self.win:
+            round = Round()
+            self.printFild(round)
+            endRoundStr = ''
 
-		while not self.win:
-			round = Round()
-			self.printFild(round)
+            while round.mishitCounter < self.maxMishits and not round.winRound:
+                round.takeLeter()
+                if round.chechRoundWin():
+                    self.result[self.player] += 1
+                    endRoundStr = '\nZgadłeś!'
+                elif round.mishitCounter == self.maxMishits:
+                    endRoundStr = '\nNiestety wisisz!'
+                self.printFild(round)
 
-			while round.mishitCounter < self.maxMishits and not round.winRound:
-				round.takeLeter()
-				if round.chechRoundWin():
-					self.result[self.player] += 1	
-					endRoundStr = '\nZgadłeś!'
-				elif round.mishitCounter == self.maxMishits:
-					endRoundStr = '\nNiestety wisisz!'
-				self.printFild(round)
-			else:
-				input(endRoundStr)
-				if self.result[self.player] == self.winResult:
-					self.win = True
-					input('Wygrywa gracz ' + str(self.player + 1))
-					self.gameStart()
-				self.changePlayer()
-					
-	def changePlayer(self):
-		self.player = int(not self.player)
-		print('\a')
+            input(endRoundStr)
+            if self.result[self.player] == self.winResult:
+                self.win = True
+                input('Wygrywa gracz ' + str(self.player + 1))
+                self.gameStart()
+            self.changePlayer()
 
-	def printFild(self,  round):
+    def changePlayer(self):
+        self.player = int(not self.player)
+        print('\a')
 
-		os.system('cls' if os.name == 'nt' else 'clear')
+    def printFild(self, round):
 
-		fild = '''[exit]: koniec gry\n
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        fild = '''[exit]: koniec gry\n
 playr 1: {}\t\t\tplayr 2: {}\n
 playr {}\t\t\t\tpudła: {}/{}
 {}\nHasło:  {}\n\nLitery: {}'''.format(
-			str(self.result[0]),  str(self.result[1]), 
-			str(self.player + 1), str(round.mishitCounter), str(self.maxMishits),
-			round.getHangman(), round.getMaskedWord(), ' '.join(sorted(round.leters))
-		)
-		print(fild)
+            str(self.result[0]), str(self.result[1]),
+            str(self.player + 1), str(round.mishitCounter), str(self.maxMishits),
+            round.getHangman(), round.getMaskedWord(), ' '.join(sorted(round.leters))
+        )
+        print(fild)
+
 
 class Round:
 
-	def __init__(self):
-		self.word = self.getRandomWord()
-		self.leters = []
-		self.mishitCounter = 0
-		self.winRound = False
+    def __init__(self):
+        self.word = self.getRandomWord()
+        self.leters = []
+        self.mishitCounter = 0
+        self.winRound = False
 
-	def getRandomWord(self):
-		words = ['taboret', 'kwiatek', 'mlotek']
-		return random.choice(words)
+    def getRandomWord(self):
+        words = ['taboret', 'kwiatek', 'mlotek']
+        return random.choice(words)
 
-	def takeLeter(self):
-		leter = input('\nPodaj literę: ')
-		if leter in ['exit', 'exit()', 'koniec']:
-			exit()
-		while leter[0] in self.leters:
-			leter = input('\nPodaj nową literę: ')
-		self.leters.append(leter[0])
+    def takeLeter(self):
+        leter = input('\nPodaj literę: ')
+        if leter in ['exit', 'exit()', 'koniec']:
+            exit()
+        while leter[0] in self.leters:
+            leter = input('\nPodaj nową literę: ')
+        self.leters.append(leter[0])
 
-		if leter not in self.word:
-			self.mishitCounter += 1
+        if leter not in self.word:
+            self.mishitCounter += 1
 
-	def chechRoundWin(self):
-		if set(list(self.word)) <= set(self.leters):
-			self.winRound = True
-		return self.winRound
+    def chechRoundWin(self):
+        if set(list(self.word)) <= set(self.leters):
+            self.winRound = True
+        return self.winRound
 
-	def getMaskedWord(self):
-		maskedWord = ''
-		for leter in self.word:
-			if leter in self.leters:
-				maskedWord += leter
-			else:
-				maskedWord += ' _ '
-		return maskedWord
+    def getMaskedWord(self):
+        maskedWord = ''
+        for leter in self.word:
+            if leter in self.leters:
+                maskedWord += leter
+            else:
+                maskedWord += ' _ '
+        return maskedWord
 
-	def getHangman(self):
+    def getHangman(self):
 
-		hangmans = [
-'''		     
-		      
-		      
-		      
-		      
-		       \n''',
-'''		     
-		      
-		      
-		      
-		      
-		    _ _\n''',
-'''		     
-		     |
-		     |
-		     |
-		     |
-		    _|_\n''',
-'''		      
-		    \|
-		     |
-		     |
-		     |
-		    _|_\n''',
-'''		 ____
-		    \|
-		     |
-		     |
-		     |
-		    _|_\n''',
-'''		 ____
-		 |  \|
-		     |
-		     |
-		     |
-		    _|_\n''',
-'''		 ____
-		 |  \|
-		 o   |
-		     |
-		     |
-		    _|_\n''',
-'''		 ____
-		 |  \|
-		 o   |
-		 |   |
-		     |
-		    _|_\n''',
-					
-'''		 ____
-		 |  \|
-		 o   |
-		/|\  |
-		     |
-		    _|_\n''',
-					
-'''		 ____
-		 |  \|
-		 o   |
-		/|\  |
-		/\   |
-		    _|_\n'''
-		]
-		return hangmans[self.mishitCounter]
-		
+        hangmans = [
+            '''		     
+                          
+                          
+                          
+                          
+                           \n''',
+            '''		     
+                          
+                          
+                          
+                          
+                        _ _\n''',
+            '''		     
+                         |
+                         |
+                         |
+                         |
+                        _|_\n''',
+            '''		      
+                        \|
+                         |
+                         |
+                         |
+                        _|_\n''',
+            '''		 ____
+                        \|
+                         |
+                         |
+                         |
+                        _|_\n''',
+            '''		 ____
+                     |  \|
+                         |
+                         |
+                         |
+                        _|_\n''',
+            '''		 ____
+                     |  \|
+                     o   |
+                         |
+                         |
+                        _|_\n''',
+            '''		 ____
+                     |  \|
+                     o   |
+                     |   |
+                         |
+                        _|_\n''',
+
+            '''		 ____
+                     |  \|
+                     o   |
+                    /|\  |
+                         |
+                        _|_\n''',
+
+            '''		 ____
+                     |  \|
+                     o   |
+                    /|\  |
+                    /\   |
+                        _|_\n'''
+        ]
+        return hangmans[self.mishitCounter]
+
 
 if __name__ == '__main__':
-	game = Game()
+    game = Game()
