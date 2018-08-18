661eeb2d2fd98365574cfe1657bb24f2ca1dcfd9
slawek <slawomirpawlowski@gmail.com>
2018-08-18 11:58:20 +0200

    typo field Field added module words added suppoort for words with space

--- hangman.py
+++ hangman.py
@@ -1,28 +1,30 @@
 import random
-from fild import Fild
+from field import Field
+from words import Words
 
 
 class Game:
-    def __init__(self, win_result, max_mishits):
+    def __init__(self, win_result, max_mishits, words):
         self.win_result = win_result
         self.max_mishits = max_mishits
+        self.words = words
         self.result = [0, 0]
         self.player = 0
         self.win = False
-        self.fild = Fild()
-        self.round = Round()
+        self.field = Field()
+        self.round = Round(self.words)
         self.game_start()
 
     def game_start(self):
 
         while not self.win:
             self.round.new_round()
-            self.fild.print_fild(self)
+            self.field.print_fild(self)
             end_round_str = ''
 
             while self.round.mishit_counter < self.max_mishits and not self.round.win_round:
                 self.round.take_letter()
-                self.fild.print_fild(self)
+                self.field.print_fild(self)
                 if self.round.check_round_win():
                     self.result[self.player] += 1
                     end_round_str = '\nZgadłeś!'
@@ -42,7 +44,8 @@ class Game:
 
 class Round:
 
-    def __init__(self):
+    def __init__(self, words):
+        self.words = words
         self.word = ''
         self.letters = []
         self.mishit_counter = 0
@@ -50,13 +53,12 @@ class Round:
 
     def new_round(self):
         self.word = self.get_random_word()
-        self.letters = []
+        self.letters = [' ']
         self.mishit_counter = 0
         self.win_round = False
 
     def get_random_word(self):
-        words = ['taboret', 'kwiatek', 'mlotek']
-        return random.choice(words)
+        return random.choice(self.words)
 
     def take_letter(self):
         letter = self.get_letter()
@@ -85,10 +87,9 @@ class Round:
     def get_masked_word(self):
         masked_word = ''
         for letter in self.word:
-            if letter in self.letters:
-                masked_word += letter
-            else:
-                masked_word += ' _ '
+            if letter not in self.letters:
+                letter = '_'
+            masked_word += ' ' + letter
         return masked_word
 
 
@@ -102,7 +103,31 @@ if __name__ == '__main__':
     win_result = input("Do ilu wygranych grancie? max (1-99)")
     while not win_result.isdecimal() or int(win_result) < 1 or int(win_result) > 99:
         win_result = input("Podaj poprawną liczbę! ")
+
     max_mishits = input("Do ile pudeł kończy rundę? (1-11) ")
     while not max_mishits.isdecimal() or int(max_mishits) < 1 or int(max_mishits) > 11:
         max_mishits = input("Podaj poprawną liczbę! ")
-    game = Game(int(win_result), int(max_mishits))
+
+    win_result, max_mishits = int(win_result), int(max_mishits)
+
+    words = Words()
+
+    max_word_len = input("jak długie mogą być słowa? (minimalnie: {} maksymalnie: {}) ".format(
+        words.min_len, words.max_len))
+
+    while not max_word_len.isdecimal() or int(max_word_len) < words.min_len or int(max_word_len) > words.max_len:
+        max_word_len = input("Podaj poprawną liczbę! ")
+    max_word_len = int(max_word_len)
+
+    min_word_len = input("jak krótnie mogą być słowa? (minimalnie: {} maksymalnie: {}) ".format(
+        words.min_len, max_word_len))
+    while not min_word_len.isdecimal() or int(min_word_len) < words.min_len or int(min_word_len) > max_word_len:
+        min_word_len = input("Podaj poprawną liczbę! ")
+
+    min_word_len = int(min_word_len)
+
+    print(min_word_len, max_word_len)
+
+    game = Game(win_result, max_mishits, words.get_words(min_word_len, max_word_len))
+
+
---
+++ words.py
@@ -0,0 +1,23 @@
+file_name = 'test_words.txt'
+
+
+class Words:
+
+    def __init__(self):
+        self.words = self.create_words_list()
+        words_len_list = [len(x) for x in self.words]
+        self.min_len = min(words_len_list)
+        self.max_len = max(words_len_list)
+
+    def create_words_list(self):
+        words = []
+        with open(file_name, 'r') as f:
+            for line in f:
+                words.append(line.rstrip())
+        return words
+
+    def get_words(self, min_len, max_len):
+        ret = list(filter(lambda x: min_len <= len(x) <= max_len, self.words))
+        print(len(ret))
+        return ret
+
